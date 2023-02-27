73d1856f3d94034c03217942276507ec725312e0
Stalkie666 <hamatlama@gmail.com>
2023-02-27 23:38:40 +0100

    error.c priprava

--- IJC/DU1/error.c
+++ IJC/DU1/error.c
@@ -2,3 +2,13 @@
    // Řešení IJC-DU1, příklad b), 22.3.2023
    // Autor: Jakub Hamadej, FIT
    // Přeloženo: gcc 9.4.0
+
+#include "error.h"
+
+void warning(const char *fmt, ...){
+   // TO-DO
+}
+
+void error_exit(const char *fmt, ...){
+   // TO-DO
+}
