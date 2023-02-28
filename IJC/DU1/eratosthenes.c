0067241a37baba53d49bce2ef24c41946b5f6223
Stalkie666 <hamatlama@gmail.com>
2023-02-28 18:10:21 +0100

    implementace Eratosthenova sita, netestovano

--- IJC/DU1/eratosthenes.c
+++ IJC/DU1/eratosthenes.c
@@ -5,7 +5,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <limits.h>
+
 #include "bitset.h"
+
+#define N_PRIME_NUM_TO_PRINT 10
 
 //projit jednotliva cisla v poli po bitech, pouzit bitovy posuv
     //filter bude unsigned long stejne jako pole.array[] 
@@ -18,5 +23,37 @@
 
 
 void Eratosthenes( bitset_t pole ){
+        for(bitset_index_t i = 1; i < ( pole[0] / (sizeof(bitset_index_t)*8) + 2 ); ++i ){
+            pole[i] = ULONG_MAX;
+        }
+        bitset_index_t tmp;
+        for( bitset_index_t i = 2; i < pole[0]; ++i ){
+            if( bitset_getbit(pole,i) == 1 ){
+                for(bitset_index_t j = i + 1; i < pole[0]; ++i){
+                    if( bitset_getbit(pole,j) % i == 0 ){
+                        bitset_setbit(pole,j,0);
+                    }
+                }
+            }
+        }
 
+        bitset_index_t primeNumbers[N_PRIME_NUM_TO_PRINT] = {0};
+
+        bitset_index_t index = N_PRIME_NUM_TO_PRINT;
+
+        bitset_index_t backwards = pole[0];
+
+        while(index && backwards > 0){
+            if( bitset_getbit(pole,backwards) ){
+                primeNumbers[index-1] = backwards;
+                index--;
+            }
+            backwards--;
+        }
+
+        for(bitset_index_t i = 0; i < N_PRIME_NUM_TO_PRINT; ++i){
+            if(primeNumbers[i] != 0){
+                printf("%d\n",primeNumbers[i]);
+            }
+        }
 }
