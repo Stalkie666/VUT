501c8dc3a440bd680b8e6d69f423d2e4fd4c1614
Stalkie666 <hamatlama@gmail.com>
2023-03-05 13:18:32 +0100

    error.c - uprava

--- IJC/DU1/error.c
+++ IJC/DU1/error.c
@@ -9,17 +9,17 @@
 #include "error.h"
 
 void warning(const char *fmt, ...){
-   // TO-DO
    va_list args;
    va_start(args, fmt);
+   fprintf(stderr, "Warning: ");
    vfprintf(stderr, fmt, args);
    va_end(args);
 }
 
 void error_exit(const char *fmt, ...){
-   // TO-DO
    va_list args;
    va_start(args, fmt);
+   fprintf(stderr, "Error: ");
    vfprintf(stderr, fmt, args);
    va_end(args);
    exit(1);
