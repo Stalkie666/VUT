565c9c5907f2a48a3b151b1e049ccce8bdc11a38
Stalkie666 <hamatlama@gmail.com>
2023-03-11 06:05:47 +0100

    primes.c + uprava Makefiel

--- IJC/DU1/primes.c
+++ IJC/DU1/primes.c
@@ -4,12 +4,21 @@
    // Přeloženo: gcc 9.4.0
 
 #include <stdio.h>
+#include <time.h>
+#include "bitset.h"
+#include "eratosthenes.h"
+
+#define VELIKOST_POLE 230000000UL
 
 int main(){
-    #ifdef USE_INLINE
-    #else
-    #endif
+    clock_t start = clock();
 
+    bitset_create(jmeno_pole,VELIKOST_POLE);
 
+    Eratosthenes(jmeno_pole);
+
+    //zde pridat logiku pro vytisknuti poslednich deseti cisel
+
+    fprintf(stderr,"Time=%.3g\n",(double)(clock()-start)/CLOCKS_PER_SEC);
     return 0;
 }
