8441a45544fd0622ae75db82a1da4d7a55a67540
Stalkie666 <hamatlama@gmail.com>
2023-03-11 06:31:08 +0100

    tisk poslednich 10 cisel

--- IJC/DU1/primes.c
+++ IJC/DU1/primes.c
@@ -4,21 +4,45 @@
    // Přeloženo: gcc 9.4.0
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include "bitset.h"
 #include "eratosthenes.h"
 
-#define VELIKOST_POLE 230000000UL
+#define ARRAY_SIZE 230000000UL
+#define NUMBERS_TO_PRINT 10
+
+void printLastPrimeNumbers(bitset_t array, bitset_index_t indexOfLastNumberOfArray, bitset_index_t numbersToPrint){
+    bitset_t tmpArr = calloc(numbersToPrint,sizeof(bitset_index_t));
+    if( tmpArr == NULL ){
+        fprintf(stderr,"Memory allocation failure!\n");
+        return;
+    }
+    bitset_index_t tmpNumToPrint = numbersToPrint;
+    
+    while(tmpNumToPrint > 0 && indexOfLastNumberOfArray > 0){
+        if( bitset_getbit(array,indexOfLastNumberOfArray) == 0 ){
+            tmpArr[tmpNumToPrint-1] = indexOfLastNumberOfArray;
+            --tmpNumToPrint;
+        }
+        --indexOfLastNumberOfArray;
+    }
+    for(bitset_index_t i = 0; i < numbersToPrint; ++i ){
+        printf("%lu\n", tmpArr[i]);
+    }
+    free(tmpArr);
+}
 
 int main(){
+    //start counting
     clock_t start = clock();
-
-    bitset_create(jmeno_pole,VELIKOST_POLE);
-
-    Eratosthenes(jmeno_pole);
-
-    //zde pridat logiku pro vytisknuti poslednich deseti cisel
-
+    //define and init array
+    bitset_create(arr,ARRAY_SIZE);
+    //getting prime numbers with eratoshenes sieve
+    Eratosthenes(arr);
+    //print last N prime numbers
+    printLastPrimeNumbers(arr, ARRAY_SIZE - 1, NUMBERS_TO_PRINT);
+    //print how much time it takes
     fprintf(stderr,"Time=%.3g\n",(double)(clock()-start)/CLOCKS_PER_SEC);
     return 0;
 }
