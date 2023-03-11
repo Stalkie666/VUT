5a3ce8a6f9f22003ab6ca6283cabff68cf413765
Stalkie666 <hamatlama@gmail.com>
2023-03-11 21:39:00 +0100

    steg castecne

--- IJC/DU1/steg-decode.c
+++ IJC/DU1/steg-decode.c
@@ -2,3 +2,17 @@
    // Řešení IJC-DU1, příklad b), 22.3.2023
    // Autor: Jakub Hamadej, FIT
    // Přeloženo: gcc 9.4.0
+
+#include <stdio.h>
+#include <limits.h>
+
+#include "eratosthenes.h"
+#include "bitset.h"
+#include "error.h"
+#include "ppm.h"
+
+int main(int argc, char *argv[]){
+   if(argc != 2) error_exit("Chymbny pocet argumentu.\n");
+
+   struct ppm * img = 
+}
