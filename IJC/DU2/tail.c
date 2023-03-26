fa54a5ea94d8f8839a9fb0e36481bbf36ec45868
Stalkie666 <hamatlama@gmail.com>
2023-03-26 21:50:47 +0200

    tail.c zakladni kostra

--- IJC/DU2/tail.c
+++ IJC/DU2/tail.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define LINE_LIMIT 4096 // 4095 printable chars + '\0'
 
@@ -25,9 +26,88 @@ CB_Struct_t * cb_create(int n){
     returnStruct->size = n;
     returnStruct->readIndex = 0;
     returnStruct->writeIndex = 0;
+    for(size_t i = 0; i < returnStruct->size; ++i){
+        returnStruct->lines[i].line = NULL;
+    }
     return returnStruct;
 }
 
-int main(){
-    return 0;
+void cb_put(CB_Struct_t * cb, char * line){
+    if(cb->lines[cb->writeIndex].line != NULL){
+        free(cb->lines[cb->writeIndex].line);
+    }
+    cb->lines[cb->writeIndex++].line = line; 
+}
+
+char * cb_get(CB_Struct_t * cb){
+    char * retVal = cb->lines[cb->readIndex++].line;
+    cb->readIndex %= cb->size;
+    return retVal;
+}
+
+void cb_free(CB_Struct_t * cb){
+    //mozna jeste uvolnovat samotne line
+    free(cb->lines);
+    free(cb);
+}
+
+//read from stream
+int readStream(FILE * stream, int allocSize){
+    CB_Struct_t * cb = cb_create(allocSize);
+    /*
+        zde kod pro zpracovani vstupu
+    */
+    cb_free(cb);
+    return 1;
+}
+
+//stdin mode
+int readStdIn(int allocSize){
+    return readStream(stdin,allocSize);
+}
+//read file mode
+int readFromFile(int allocSize, char * fileName){
+    FILE * stream = fopen(fileName,"r");
+    if(!stream){
+        fprintf(stderr,"Otevreni souboru selhalo.\n");
+        return 0;
+    }
+    if( !readStream(stream,allocSize) ){
+        fclose(stream);
+        return 0
+    }
+    else{
+        fclose(stream);
+    }
+    return 1;
+}
+
+
+
+int main(int argc, char *argv[]){
+    if(argc > 4)
+        fprintf(stderr,"Prilis mnoho argumentu.\n");
+    int CB_allocSize = 10;
+    char * fileName = NULL;
+    for(size_t i = 1; i < argc; ++i){
+        if( strcmp(argv[i],"-n") && (i+1) < argc ){
+            CB_allocSize = atoi(argv[i+1]);
+            if( CB_allocSize <= 0 ){
+                fprintf(stderr,"Zadana nespravna velikost CB, nebo uplny nesmysl.\n");
+                return EXIT_FAILURE;
+            }
+        }
+        else{
+            fileName = argv[i];
+        }
+    }
+
+    if( fileName == NULL ){
+        //zde zavolat funkci pro cteni ze stdin
+    }
+    else{
+        //zde funkce pro cteni ze souboru
+    }
+
+    return EXIT_SUCCESS;
 }
