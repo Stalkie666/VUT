bcb22a0900919e560048d06a3469a97062754715
Stalkie666 <hamatlama@gmail.com>
2023-04-01 21:59:35 +0200

    htab_private add

---
+++ IJC/DU2/htab_private.h
@@ -0,0 +1,22 @@
+#ifndef HTAB_PRIVATE_H__
+#define HTAB_PRIVATE_H__
+
+
+#include "htab.h"
+
+typedef struct htab_item{
+    htab_pair_t pair;
+    struct htab_item *next;
+}htab_item_t;
+
+struct htab{
+    int size;
+    int arr_size;
+    htab_item_t ** items;
+};
+
+htab_item_t * htab_init_item();
+
+void htab_free_item(htab_item_t * item);
+
+#endif
