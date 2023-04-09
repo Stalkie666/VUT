f5b57dcff278e62db6676944bc6c4a8b1e486748
Stalkie666 <hamatlama@gmail.com>
2023-04-09 22:15:04 +0200

    htab erase done

---
+++ IJC/DU2/htab_erase.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "htab_private.h"
+
+bool htab_erase(htab_t * t, htab_key_t key){
+    size_t index = htab_hash_function(key);
+
+    for( htab_item_t * item = t->arr_ptr[index]; item != NULL; item = item->next  ){
+        if( !strcmp( item->pair.key, key ) ){
+            htab_item_t * forErase = item;
+            item = forErase->next;
+            htab_free_item(forErase);
+            t->size--;
+            return true;
+        }
+    }
+
+
+    return false;
+}
