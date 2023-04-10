41acd27dd7811da4eecac5c33741e341726e32a5
Stalkie666 <hamatlama@gmail.com>
2023-04-10 14:41:59 +0200

    oprava

--- IJC/DU2/htab_erase.c
+++ IJC/DU2/htab_erase.c
@@ -1,7 +1,7 @@
 #include "htab_private.h"
 
 bool htab_erase(htab_t * t, htab_key_t key){
-    size_t index = htab_hash_function(key);
+    size_t index = htab_hash_function(key) % t->arr_size;
 
     for( htab_item_t * item = t->arr_ptr[index]; item != NULL; item = item->next  ){
         if( !strcmp( item->pair.key, key ) ){
