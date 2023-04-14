f58df17b5ffcbd29be5c27e17cbc0d1107b94f16
Stalkie666 <hamatlama@gmail.com>
2023-04-14 16:48:16 +0200

    uprava

--- IJC/DU2/htab_init_item.c
+++ IJC/DU2/htab_init_item.c
@@ -6,15 +6,14 @@ htab_item_t * htab_init_item(htab_key_t key){
     if(!retVal)
         return NULL;
 
-    // int allocLen = strlen(key) + 1;
-    // retVal->pair.key = malloc(allocLen * sizeof(char));
-    // if( !(retVal->pair.key) ){
-    //     free(retVal);
-    //     return NULL;
-    // }
-
-    // strcpy((char*)retVal->pair.key, key);
-    retVal->pair.key = key;
+    int allocLen = strlen(key) + 1;
+    retVal->pair.key = malloc(allocLen * sizeof(char));
+    if( !(retVal->pair.key) ){
+        free(retVal);
+        return NULL;
+    }
+    strcpy((char*)retVal->pair.key, key);
+    //retVal->pair.key = key;
 
     retVal->pair.value = 0;
     retVal->next = NULL;
