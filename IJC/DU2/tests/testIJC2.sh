96dbc8fce472ae9a6d4cf24e336337dab41a36a7
Stalkie666 <hamatlama@gmail.com>
2023-04-18 22:38:02 +0200

    Minnor changes + Makefile run

--- IJC/DU2/tests/testIJC2.sh
+++ IJC/DU2/tests/testIJC2.sh
@@ -1,31 +1,42 @@
 #!/bin/bash
-
+echo "----------------------------TESTOVANI---------------------------"
+# Testy pro tail
 echo "Test: Tail"
-echo "################################################################"
-
 ./tail -n 10 < tests/testTail.txt | tee /tmp/testTail.txt
 tail -n 10 tests/testTail.txt > /tmp/tail.txt
+echo "----------------------------------------------------------------"
 if diff /tmp/testTail.txt /tmp/tail.txt
 then
-    echo "------------------------------------------------------------"
     echo "Vysledek je indenticky s prikazem tail"
+else
+    echo "Neodpovida testovacimu vystupu"
 fi
 
 echo "################################################################"
+
+# Referencni reseni wordcount
 ./wordcount- < tests/testWordCount.txt | sort > /tmp/originalWordCount.txt
+
+# Test pro staticky wordcount
 echo "Test: wordcount"
 ./wordcount < tests/testWordCount.txt | sort | tee /tmp/testWordCount.txt
+    echo "----------------------------------------------------------------"
 if diff /tmp/testTail.txt /tmp/tail.txt
 then
-    echo "------------------------------------------------------------"
     echo "Vystup wordcount je indenticky s vystupem wordcount-"
+else
+    echo "Neodpovida testovacimu vystupu"
 fi
+
+# Test pro dynamicky wordcount
 echo "################################################################"
 echo "Test: wordcount-dynamic"
 export LD_LIBRARY_PATH="."
 ./wordcount-dynamic < tests/testWordCount.txt | sort | tee /tmp/testWordCount.txt
+echo "----------------------------------------------------------------"
 if diff /tmp/testTail.txt /tmp/tail.txt
 then
-    echo "------------------------------------------------------------"
     echo "Vystup wordcount-dynamic je indenticky s vystupem wordcount-"
+else
+    echo "Neodpovida testovacimu vystupu"
 fi
