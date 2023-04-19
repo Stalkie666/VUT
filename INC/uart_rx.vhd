e742b81c086609d4ee5ddaae65bdb1895a895301
Stalkie666 <hamatlama@gmail.com>
2023-04-20 00:25:23 +0200

    startbit a counter15

--- INC/uart_rx.vhd
+++ INC/uart_rx.vhd
@@ -23,30 +23,62 @@ end entity;
 -- Architecture implementation (INSERT YOUR IMPLEMENTATION HERE)
 architecture behavioral of UART_RX is
     --vnejsi dratovani, mozna jeste upravim
-    signal rstStart : std_logic := '0';
-    signal cnt15 : std_logic := '0';
-    signal cnt8 : std_logic := '0';
-    signal dCnt10 : std_logic := '0';
-    signal qCnt10 : std_logic := '0';
-    signal endBit : std_logic := '0';
+    signal rstStart : std_logic := '0'; --vystup ze startBit
+    signal cnt15 : std_logic := '0'; -- vystup ze cnt15 pro cnt10
+    signal cnt8 : std_logic := '0'; -- vystup ze cnt15 pro shift register
+    signal dCnt10 : std_logic := '0'; -- vystup z cnt10 do delay
+    signal qCnt10 : std_logic := '0'; -- vystup z delay do rst startBit a na DOUT_VLD
+    signal endBit : std_logic := '0'; -- vystup z shift registru do DOUT_VLD skrz and spolecne s qcnt10
+    --vnitrni signaly jednotlivych procesu
+    signal count15 : std_logic_vector (3 downto 0) := "0000";
 begin
 
     -- Instance of RX FSM
     fsm: entity work.UART_RX_FSM
     port map (
         CLK => CLK,
-        RST => RST
+        RST => RST,
+        DIN => DIN,
+        DOUT => DOUT,
+        DOUT_VLD => DOUT_VLD
+
     );
 
     DOUT <= (others => '0');
-    DOUT_VLD <= '0';
+    DOUT_VLD <= qCnt10 and endBit;
 
     -- START_BIT
     startBit: process(CLK,RST,qCnt10) begin
+        if RST = '1' or qCnt10 = '1' then
+            rstStart <= '0';
+        elsif rising_edge(CLK) then
+            if DIN = '1' and rstStart = '0' then
+                rstStart <= '1';
+            else
+                rstStart <= '0';
+            end if;
+        end if;
     end process startBit;
 
     -- COUNTER15
     counter15: process(CLK, rstStart) begin
+        if rstStart = '0' then
+            count15 <= "0000";
+            cnt8 <= '0';
+            cnt15 <= '0';
+        elsif rising_edge(CLK) then
+            if count15 = "0111" then
+                cnt8 <= '1';
+                cnt15 <= '0';
+            elsif count15 = "1111" then
+                cnt8 <= '0';
+                cnt15 <= '1';
+            else
+                cnt8 <= '0';
+                cnt15 <= '1';
+            end if;
+            count15 <= std_logic_vector( unsignef(count) + 1 );
+        end if;
     end process counter15;
     
     --SHIFT_REGISTER
