b3f7532ac51a9bb8c9f4ff16ef03210a341ed30c
Stalkie666 <hamatlama@gmail.com>
2023-04-22 00:36:18 +0200

    knihovna

--- INC/uart_rx.vhd
+++ INC/uart_rx.vhd
@@ -5,6 +5,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 use ieee.std_logic_arith.all;
+use ieee.numeric_std.all
 
 
 -- Entity declaration (DO NOT ALTER THIS PART!)
