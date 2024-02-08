3d36ec3d6ef042e5eb13a9d2d6a44d1507999a33
Stalkie666 <hamatlama@gmail.com>
2024-02-08 23:49:08 +0100

    creating testFile

--- IPP/project1/parse.py
+++ IPP/project1/parse.py
@@ -1,22 +1,41 @@
 import xml.etree.ElementTree as ET
+import xml.dom.minidom as MD
+import sys
 
 #--help
 def help():
     print("printuju help")
+    sys.exit(0)
+
 
 
 
 #MAIN
+    
+#check for incomming arguments    
+n =  len(sys.argv)   
+if n > 1:
+    if sys.argv[1] == "--help":
+        help()
+    else:
+        print("Unknown argument")
+        sys.exit(10)
+
+firstLine = sys.stdin.readline()
+if firstLine.lower() != ".ippcode24\n":
+    print("Spatna hlavicka: " + firstLine.lower())
+    sys.exit(0)
 
 
-header = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
-a = ET.Element(header)
+# header = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+header = ET.Element("xml",version="1.0",encoding="UTF-8")
+print(ET.tostring(header))
+
+header = ET.Element("Program")
+a = ET.SubElement(header,"MOV")
 
 
-
-
-
-#nacist vstup - nacitat po radcich (asi getline? - nebo jeji python ekvivalent)
+#nacist vstup - nacitat po radcich (asi getline? - nebo jeji python ekvivalent - jedna se o 'sys.stdin.readline()' )
 
 #zkontrolovat vstup
     #nacist prvni slovo na radku, podle toho jednat - dobra zprava, na nejake alokace pameti muzu srat
@@ -25,5 +44,5 @@ a = ET.Element(header)
         #pokud by se jednolo o komentar od zacatku tak muzeme zahodit, neni potreba
 
 #vyplivnout vystup pokud je na vstupu vsechno v poradku - nastudovat jak ma to XML vypadat
-ET.dump(a) #asi takto
-help()
+#ET.dump(header) #asi takto
+#print(ET.tostring(header))
