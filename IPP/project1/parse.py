fbe4ead357df2b7d208eb8182ea7d98f435f2c3b
Stalkie666 <hamatlama@gmail.com>
2024-02-18 18:56:36 +0100

    reading from input

--- IPP/project1/parse.py
+++ IPP/project1/parse.py
@@ -1,5 +1,5 @@
 import xml.etree.ElementTree as ET
-import xml.dom.minidom as MD
+import xml.dom.minidom as minidom
 import sys
 
 #--help
@@ -7,7 +7,10 @@ def help():
     print("printuju help")
     sys.exit(0)
 
-
+def getArrOfStrings(line):
+    arr = line.split("#",1)
+    arr = arr[0].split()
+    return arr
 
 
 #MAIN
@@ -26,23 +29,15 @@ if firstLine.lower() != ".ippcode24\n":
     print("Spatna hlavicka: " + firstLine.lower())
     sys.exit(0)
 
-
-# header = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
-header = ET.Element("xml",version="1.0",encoding="UTF-8")
-print(ET.tostring(header))
-
-header = ET.Element("Program")
-a = ET.SubElement(header,"MOV")
+for line in sys.stdin:
+    arr = getArrOfStrings(line)
+    if arr:
+        print(arr)
+    
 
 
-#nacist vstup - nacitat po radcich (asi getline? - nebo jeji python ekvivalent - jedna se o 'sys.stdin.readline()' )
 
-#zkontrolovat vstup
-    #nacist prvni slovo na radku, podle toho jednat - dobra zprava, na nejake alokace pameti muzu srat
-        #behem prochazeni kontrolovat jestli nenarazime predcasne (nebo prilis pozde, muze tam byt o operand navic) 
-                                                                            #na komentar, muze byt pouze jednoradkovi
-        #pokud by se jednolo o komentar od zacatku tak muzeme zahodit, neni potreba
 
-#vyplivnout vystup pokud je na vstupu vsechno v poradku - nastudovat jak ma to XML vypadat
-#ET.dump(header) #asi takto
-#print(ET.tostring(header))
+
+
+#vyplivnout vystup pokud je na vstupu vsechno v poradku - v parse_kopie.py
