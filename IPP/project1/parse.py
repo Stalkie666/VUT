ddb47888ec4f2bbad5fd7947d746cc2b248e59af
Stalkie666 <hamatlama@gmail.com>
2024-02-22 13:05:07 +0100

    creating table of instructions

--- IPP/project1/parse.py
+++ IPP/project1/parse.py
@@ -11,44 +11,90 @@ def getArrOfStrings(line):
     arr = line.split("#",1)
     arr = arr[0].split()
     retVal = []
-    for i, a in enumerate(arr):
-        arr[i] = a.replace('<','&lt;').replace('>','&gt;').replace('&','&amp;')
+    #tohle pry nepotrebuju, ET to udela za mne, neverim jim, takze zalohu sem
+    #for i, a in enumerate(arr):
+    #   arr[i] = a.replace('<','&lt;').replace('>','&gt;').replace('&','&amp;')
     return arr
 
+#zpracovani radky do xml tree
 def addLineIntoXMLTree(root,inputArr):
     instruction_node = ET.SubElement(root,inputArr[0])
+    
+    #asi to nacpat do pole poli jednotlive instrukce a potom s tim neco vymyslet
+
     #MOVE ⟨var⟩ ⟨symb⟩ 
-    #CREATEFRAME
-    #PUSHFRAME 
-    #POPFRAME
+    #INT2CHAR ⟨var⟩ ⟨symb⟩
+    #STRLEN ⟨var⟩ ⟨symb⟩
+    #TYPE ⟨var⟩ ⟨symb⟩
+
     #DEFVAR ⟨var⟩
-    #CALL ⟨label⟩ 
-    #RETURN 
-    #PUSHS ⟨symb⟩
     #POPS ⟨var⟩
+
+    #CALL ⟨label⟩
+    #LABEL ⟨label⟩ 
+    #JUMP ⟨label⟩
+    
+    #PUSHS ⟨symb⟩
+    #WRITE ⟨symb⟩
+    #EXIT ⟨symb⟩
+    #DPRINT ⟨symb⟩ 
+    
     #ADD ⟨var⟩ ⟨symb1⟩ ⟨symb2⟩
     #SUB ⟨var⟩ ⟨symb1⟩ ⟨symb2⟩
     #MUL ⟨var⟩ ⟨symb1⟩ ⟨symb2⟩
     #IDIV ⟨var⟩ ⟨symb1⟩ ⟨symb2⟩
     #LT/GT/EQ ⟨var⟩ ⟨symb1⟩ ⟨symb2⟩ 
-    #AND/OR/NOT ⟨var⟩ ⟨symb1⟩ ⟨symb2⟩ 
-    #INT2CHAR ⟨var⟩ ⟨symb⟩
-    #STRI2INT ⟨var⟩ ⟨symb1⟩ ⟨symb2⟩ 
-    #READ ⟨var⟩ ⟨type⟩
-    #WRITE ⟨symb⟩
-    #CONCAT ⟨var⟩ ⟨symb1⟩ ⟨symb2⟩ 
-    #STRLEN ⟨var⟩ ⟨symb⟩
+    #AND/OR/NOT ⟨var⟩ ⟨symb1⟩ ⟨symb2⟩
+    #STRI2INT ⟨var⟩ ⟨symb1⟩ ⟨symb2⟩
+    #CONCAT ⟨var⟩ ⟨symb1⟩ ⟨symb2⟩
     #GETCHAR ⟨var⟩ ⟨symb1⟩ ⟨symb2⟩ 
-    #SETCHAR ⟨var⟩ ⟨symb1⟩ ⟨symb2⟩ 
-    #TYPE ⟨var⟩ ⟨symb⟩
-    #LABEL ⟨label⟩ 
-    #JUMP ⟨label⟩
+    #SETCHAR ⟨var⟩ ⟨symb1⟩ ⟨symb2⟩
     #JUMPIFEQ ⟨label⟩ ⟨symb1⟩ ⟨symb2⟩
     #JUMPIFNEQ ⟨label⟩ ⟨symb1⟩ ⟨symb2⟩ 
-    #EXIT ⟨symb⟩
-    #DPRINT ⟨symb⟩ 
-    #BREAK 
-
+    
+    
+    
+    
+    
+    
+    
+arrayOfCommands = [
+    ['MOVE','var','symb'],
+    ['CREATEFRAME'],
+    ['PUSHFRAME'],
+    ['POPFRAME'],
+    ['DEFVAR','var'],
+    ['CALL','label'],
+    ['RETURN'],
+    ['PUSHS','symb'],
+    ['POPS','var'],
+    ['ADD', 'var','symb','symb'],
+    ['SUB', 'var','symb','symb'],
+    ['MUL', 'var','symb','symb'],
+    ['IDIV','var','symb','symb'],
+    ['LT',  'var','symb','symb'],
+    ['GT',  'var','symb','symb'],
+    ['EQ',  'var','symb','symb'],
+    ['AND', 'var','symb','symb'],
+    ['OR',  'var','symb','symb'],
+    ['NOT', 'var','symb','symb'],
+    ['INT2CHAR','var','symb'],
+    ['STRING2INT','var','symb','symb'],
+    ['READ','var','type'], #type = {int,string,bool}
+    ['WRITE','symb'],
+    ['CONCAT','var','symb','symb'],
+    ['STRLEN','var','symb'],
+    ['GETCHAR','var','symb','symb'],
+    ['SETCHAR','var','symb','symb'],
+    ['TYPE','var','symb'],
+    ['LABEL','label'],
+    ['JUMP','label'],
+    ['JUMPIFEQ', 'label','symb','symb'],
+    ['JUMPIFNEQ','label','symb','symb'],
+    ['EXIT','symb'],
+    ['DPRINT','symb'],
+    ['BREAK']
+]
 
 
 #MAIN
@@ -74,9 +120,11 @@ for line in sys.stdin:
     arr = getArrOfStrings(line)
     if arr:
         addLineIntoXMLTree(root,arr)
+        #print(arr)
     
 
 #vyplivnout vystup pokud je na vstupu vsechno v poradku - v parse_kopie.py
 tree = ET.ElementTree(root)
-tree_str = ET.tostring(root,encoding="utf-8",xml_declaration=True).decode()
+tree_str = ET.tostring(root,encoding="utf-8",xml_declaration=True,method='xml').decode('utf-8')
+tree_str = minidom.parseString(tree_str).toprettyxml(indent=f"{' ' * 4}",encoding='UTF-8').decode('utf-8')
 sys.stdout.write(tree_str)
