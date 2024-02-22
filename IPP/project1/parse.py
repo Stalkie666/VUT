254e911c7a6abe7479413ffef4f8e8e2b418f32c
Stalkie666 <hamatlama@gmail.com>
2024-02-22 16:50:06 +0100

    done alpha version, i hope

--- IPP/project1/parse.py
+++ IPP/project1/parse.py
@@ -1,6 +1,7 @@
 import xml.etree.ElementTree as ET
 import xml.dom.minidom as minidom
 import sys
+import re
 
 #--help
 def help():
@@ -9,55 +10,89 @@ def help():
 
 def getArrOfStrings(line):
     arr = line.split("#",1)
-    arr = arr[0].split()
-    retVal = []
-    #tohle pry nepotrebuju, ET to udela za mne, neverim jim, takze zalohu sem
-    #for i, a in enumerate(arr):
-    #   arr[i] = a.replace('<','&lt;').replace('>','&gt;').replace('&','&amp;')
-    return arr
+    return arr[0].split()
+
+def varXML  (instruction_node,argN,data):
+    if str(data).split('@',1)[0] in ['GF','TF','LF']:
+        node = ET.SubElement(instruction_node,argN,type='var')
+        node.text = data
+    else:
+        sys.exit(23)
+
+def symbXML (instruction_node,argN,data):
+    if str(data).split('@',1)[0] in ['GF','TF','LF']:
+        node = ET.SubElement(instruction_node,argN,type='var')
+        patern = r'^[a-zA-Z0-9_\-&%*$!?]+$'
+        if re.search(patern,str(data).split('@',1)[1]):
+            node.text = data
+        else:
+            print(str(data).split('@',1)[1])
+            sys.exit(23)
+    elif str(data).split('@',1)[0] in ['int','string','bool','nil']:
+        #zde jeste kontrolovat ze kdyz je to int tak tam fakt cpu int atd
+        node = ET.SubElement(instruction_node,argN,type=str(data).split('@',1)[0])
+        #node.text = str(data).split('@',1)[1]
+        type = str(data).split('@',1)[0]
+        constant = str(data).split('@',1)[1]
+        if type == 'int' and re.search(r'^[-]?[0-9]+$',constant):
+            node.text = constant
+        elif (type == 'bool' and (constant == 'true' or constant == 'false')):
+            node.text = constant
+        elif type == 'nil' and constant == 'nil':
+            node.text = 'nil'
+        elif type == 'string':
+            node.text = constant
+        else:
+            sys.exit(23)    
+    else:
+        sys.exit(23)
+
+def labelXML(instruction_node,argN,data):
+    patern = r'^[a-zA-Z0-9_\-&%*$!?]+$'
+    if re.search(patern,data):
+        node = ET.SubElement(instruction_node,argN,type='label')
+        node.text = data
+    else:
+        sys.exit(23)
+
+def typeXML (instruction_node,argN,data):
+    if str(data).lower() in ['int','string','bool']:
+        node = ET.SubElement(instruction_node,argN,type='type')
+        node.text = data
+    else:
+        sys.exit(23)
 
 #zpracovani radky do xml tree
-def addLineIntoXMLTree(root,inputArr):
-    instruction_node = ET.SubElement(root,inputArr[0])
-    
-    #asi to nacpat do pole poli jednotlive instrukce a potom s tim neco vymyslet
+def addLineIntoXMLTree(root,inputArr,order):
+    isfound = False
+    for item in arrayOfCommands:
+        if item[0] == inputArr[0]:
+            instruction_node = ET.SubElement(root,"instruction",order=str(order),opcode=inputArr[0])
+            isfound = True
 
-    #MOVE ⟨var⟩ ⟨symb⟩ 
-    #INT2CHAR ⟨var⟩ ⟨symb⟩
-    #STRLEN ⟨var⟩ ⟨symb⟩
-    #TYPE ⟨var⟩ ⟨symb⟩
+            #kontrola počtu parametrů
+            if len(item) != len(inputArr):
+                print('Fucking spatna delka prikazu')
+                print(inputArr)
+                sys.exit(23)
+            #for-cyklus pro vytvareni a kontrolu jednotlivych parametru
+            for i in range(1,len(inputArr)):
+                argN = "arg" + str(i)
+                if item[i] == 'var':
+                    varXML(instruction_node,argN,inputArr[i])
+                elif item[i] == 'symb':
+                    symbXML(instruction_node,argN,inputArr[i])
+                elif item[i] == 'label':
+                    labelXML(instruction_node,argN,inputArr[i])
+                else:
+                    typeXML(instruction_node,argN,inputArr[i])
+            break
+    if isfound == False:
+        print('Fucking chyba: ')
+        print(inputArr)
+        sys.exit(22)
+    
 
-    #DEFVAR ⟨var⟩
-    #POPS ⟨var⟩
-
-    #CALL ⟨label⟩
-    #LABEL ⟨label⟩ 
-    #JUMP ⟨label⟩
-    
-    #PUSHS ⟨symb⟩
-    #WRITE ⟨symb⟩
-    #EXIT ⟨symb⟩
-    #DPRINT ⟨symb⟩ 
-    
-    #ADD ⟨var⟩ ⟨symb1⟩ ⟨symb2⟩
-    #SUB ⟨var⟩ ⟨symb1⟩ ⟨symb2⟩
-    #MUL ⟨var⟩ ⟨symb1⟩ ⟨symb2⟩
-    #IDIV ⟨var⟩ ⟨symb1⟩ ⟨symb2⟩
-    #LT/GT/EQ ⟨var⟩ ⟨symb1⟩ ⟨symb2⟩ 
-    #AND/OR/NOT ⟨var⟩ ⟨symb1⟩ ⟨symb2⟩
-    #STRI2INT ⟨var⟩ ⟨symb1⟩ ⟨symb2⟩
-    #CONCAT ⟨var⟩ ⟨symb1⟩ ⟨symb2⟩
-    #GETCHAR ⟨var⟩ ⟨symb1⟩ ⟨symb2⟩ 
-    #SETCHAR ⟨var⟩ ⟨symb1⟩ ⟨symb2⟩
-    #JUMPIFEQ ⟨label⟩ ⟨symb1⟩ ⟨symb2⟩
-    #JUMPIFNEQ ⟨label⟩ ⟨symb1⟩ ⟨symb2⟩ 
-    
-    
-    
-    
-    
-    
-    
 arrayOfCommands = [
     ['MOVE','var','symb'],
     ['CREATEFRAME'],
@@ -111,19 +146,21 @@ if n > 1:
 firstLine = sys.stdin.readline().split("#",1)[0].split()
 if  len(firstLine) != 1 and firstLine[0].lower() != ".ippcode24":
     print("Spatna hlavicka: " + firstLine.lower())
-    sys.exit(0)
+    sys.exit(21)
 
 #decompose input in array of strings and get rid of comments in given code
 root = ET.Element('program')
 root.set("language","IPPcode24")
+
+order = 1
 for line in sys.stdin:
     arr = getArrOfStrings(line)
     if arr:
-        addLineIntoXMLTree(root,arr)
-        #print(arr)
+        addLineIntoXMLTree(root,arr,order)
+        order += 1
     
 
-#vyplivnout vystup pokud je na vstupu vsechno v poradku - v parse_kopie.py
+#vyplivnout vystup pokud je na vstupu vsechno v poradku
 tree = ET.ElementTree(root)
 tree_str = ET.tostring(root,encoding="utf-8",xml_declaration=True,method='xml').decode('utf-8')
 tree_str = minidom.parseString(tree_str).toprettyxml(indent=f"{' ' * 4}",encoding='UTF-8').decode('utf-8')
