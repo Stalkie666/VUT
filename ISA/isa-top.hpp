f357d4d4ec80665bb612cc0a827980d285f1e618
Stalkie666 <hamatlama@gmail.com>
2024-10-05 01:23:43 +0200

    first analyses

--- ISA/isa-top.hpp
+++ ISA/isa-top.hpp
@@ -1,7 +1,21 @@
 #include <iostream>
 #include <vector>
+#include <memory>
 #include <ncurses.h>
 #include <pcap/pcap.h>
+
+class Record{
+    public:
+    private:
+        //src ip
+        //src port
+        //dst ip
+        //dst port
+        // protocol name
+        // some Rx shit
+        // some Tx shit
+};
+
 
 
 class IsaTop{
@@ -12,5 +26,7 @@ class IsaTop{
         int listeningInterface;
         // TRUE if sort by bytes/s, FALSE if sort by packets/s
         bool sortByBytes;
+        // store all records of captured packets
+        std::vector<std::shared_ptr<Record>> records;
 
 };
