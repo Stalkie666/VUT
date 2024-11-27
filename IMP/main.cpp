5ed5c62fa5e0b928f8d641c8882fb202a745dd7f
Stalkie666 <hamatlama@gmail.com>
2024-11-27 14:32:54 +0100

    IMS project initial

---
+++ IMP/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "simlib.h"
+
+
+/**
+ * sources of places where convoys can go only one way; in other places, convoys can go both ways simuntaneously
+ * TODO: promyslet, jestli z toho neudelat fronty (Queue) a mozna i z tech konvoju
+ */
+Facility Narrow_PortSaid_Ismaille;
+Facility Narrow_BitterLake_Suez;
+
+//Convoys - because of international trade, two convoyes goes south from Port Said and one north from Suez
+Facility FirstNorthConvoy;
+Facility SecondNorthConvoy;
+Facility SouthConvoy;
+
+
+int main(){
+    return 0;
+}
